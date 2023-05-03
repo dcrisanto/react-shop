336f4f5b7f2f374a0883b466924ef08e451d067c
dcrisanto <dorelly.crisanto@gmail.com>
2023-05-03 16:41:16 -0500

    ..-

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HasRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
 import Login from '../pages/Login';
 import RecoveryPassword from '../pages/RecoveryPassword';
