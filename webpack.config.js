38a01db5303dc2035b7dcb6d0cacee6bea9572fc
dcrisanto <dorelly.crisanto@gmail.com>
2023-05-02 23:47:27 -0500

    modificación en directory devServer

--- webpack.config.js
+++ webpack.config.js
@@ -71,7 +71,7 @@ module.exports = {
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'),
+      directory: path.join(__dirname, 'dist'),
       watch: true,
     },
     watchFiles: path.join(__dirname, "./**"), //observa los cambios en todos nuestros archivos y actualiza el navegador
