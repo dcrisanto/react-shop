94c44f1570f0d4edfe8cb006c2ba142d0d67356f
dcrisanto <dorelly.crisanto@gmail.com>
2023-05-03 22:14:02 -0500

    Updates

---
+++ x.js
@@ -0,0 +1,11 @@
+<Route
+  element={
+    <div>
+      <h1>Layout</h1>
+      <Outlet />
+    </div>
+  }
+>
+  <Route path="/" element={<h2>Home</h2>} />
+  <Route path="/about" element={<h2>About</h2>} />
+</Route>
